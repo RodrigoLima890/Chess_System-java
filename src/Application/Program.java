0d57047b86940406fa95a8647a27d18d8df19c94
RodrigoLima890 <rodrigodaschagas1234@gmail.com>
2022-11-15 17:28:04 -0300

    Position class add

---
+++ src/Application/Program.java
@@ -0,0 +1,10 @@
+package Application;
+
+import Board.Position;
+
+public class Program {
+    public static void main(String[] args) {
+        Position pos = new Position(2,4);
+        System.out.println(pos.toString());
+    }
+}
